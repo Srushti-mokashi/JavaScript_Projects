04091c8b9b12eee599fd529a362b12a88f7dff3c
Srushti mokashi <srushtimokashi5@gmail.com>
2025-09-23 22:46:32 +0530

    Added Emoji Rating

---
+++ Emoji Rating/script.js
@@ -0,0 +1,26 @@
+const starsEl = document.querySelectorAll(".fa-star");
+const emojisEl =document.querySelectorAll(".far");
+const colorsArray = ["red", "orange", "lightblue", "lightgreen", "green"];
+
+updateRating(0);
+
+starsEl.forEach((starsEl, index) => {
+    starsEl.addEventListener("click", () => {
+        updateRating(index);
+    });
+});
+
+function updateRating(index){
+    starsEl.forEach((starEl, idx) => {
+        if(idx < index + 1){
+            starEl.classList.add("active");
+        } else {
+            starEl.classList.remove("active");
+        }
+    });
+
+    emojisEl.forEach((emojisEl) => {
+        emojisEl.style.transform = `translateX(-${index * 50}px)`;
+        emojisEl.style.color = colorsArray[index];
+    });
+}
